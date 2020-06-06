804d6aa22c53aebff8b0fd4c67af4085f809c43b
Flora Petersen <floramarina@comcast.net>
2020-06-06 19:00:25 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,11 +4,12 @@
 def key_for_min_value(name_hash)
   if hash_name.size == 0
     nil
+  end
   name_hash.first = min_key, min_value
   name_hash.each do |key, value|
     if value < min_value
       min_key = key
     end
   end
-  return min_key          
+  return min_key
 end
