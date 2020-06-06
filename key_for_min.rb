a3bba1cc829d50d273f822f715a44740c887bf47
Flora Petersen <floramarina@comcast.net>
2020-06-06 19:02:02 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,12 +1,12 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
-  if hash_name.size == 0
+def key_for_min_value(hash)
+  if hash.size == 0
     nil
   end
-  name_hash.first = min_key, min_value
-  name_hash.each do |key, value|
+  hash.first = min_key, min_value
+  hash.each do |key, value|
     if value < min_value
       min_key = key
     end
