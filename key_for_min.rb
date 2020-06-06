d1f9bed7ace59b4a68c6ba6a62b959499ff29d3d
Flora Petersen <floramarina@comcast.net>
2020-06-06 18:59:52 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  if hash_name.size == 0
+    nil
+  name_hash.first = min_key, min_value
+  name_hash.each do |key, value|
+    if value < min_value
+      min_key = key
+    end
+  end
+  min_key          
 end
